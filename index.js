fe19993c7e8b91c2874731a1e0a545e95627aa98
Apple <apple@Apples-MacBook-Pro-88.local>
2019-08-02 10:49:40 -0700

    Cool

---
+++ index.js
@@ -0,0 +1,23 @@
+var program = $('.background5');
+console.log(program);
+
+var program2 = $('.scene2');
+console.log(program2);
+
+var text = $('.program-text');
+console.log(text);
+
+var pogramType = $('.scene1');
+
+var programButton = $('.program');
+// console.log(popcorn);
+
+programButton.on('click', doSomething);
+
+
+function doSomething() {
+  event.preventDefault();
+ programType = program2;
+ var messageT = ["It's complicated",
+
+}
